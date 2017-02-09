1920af1f573f4b6eec461f37ff136425d8fe1faa
ramzi <ramzi.abbyad@gmail.com>
2017-02-09 07:19:21 -0600

    more progress on bash

--- bash/recursive_tree_fast.sh
+++ bash/recursive_tree_fast.sh
@@ -4,16 +4,26 @@ NCOLS=100
 
 updateIndices()
 {
-	declare -a colIndexNew
+	#declare -a colIndexNew
 	L=${#colIndex[@]}
-	colIndexNewK=0
-	for ((colIndexK=0; colIndexK < L; colIndexK++))
+	#colIndexNewK=0
+	#local colIndexSecond=0
+		printf "before loop\n"
+		printf "%s\n" ${colIndex[@]}
+	local startIndex=1
+	for ((colIndexK=0; colIndexK < L; colIndexK+=1))
 	do
-		colIndexNew[$colIndexNewK]=$(( ${colIndex[$colIndexK]} - 1 ))
-		colIndexNew[$colIndexNewK+1]=$(( ${colIndex[$colIndexK]} + 1 ))
-		((colIndexNewK+=2))
+		((startIndex=2*colIndexK+1))
+		colIndex=("$(( colIndex[2*colIndexK] - 1))" "$(( colIndex[2*colIndexK] + 1))" "${colIndex[@]:$startIndex}")
+		printf "next itr\n"
+		printf "%s\n" ${colIndex[@]}
+		#colIndex[$colIndexNewK]=$((  $colIndexOld - 1 ))
+		#colIndex[$(( $colIndexNewK+1 ))]=$((  + 1 ))
+		#((colIndexNewK+=2))
 	done
-	colIndex=("${colIndexNew[@]}")
+	#colIndex=("${colIndexNew[@]}")
+	#printf "updated indices to: \n"
+	#printf "%s\n" ${colIndex[@]}
 }
 
 adjustIndices()
@@ -22,32 +32,34 @@ adjustIndices()
 	for ((colIndexK=0; colIndexK < L; colIndexK+=2))
 	do
 		colIndex[$colIndexK]=$(( ${colIndex[$colIndexK]} - 1))
-		colIndex[$colIndexK+1]=$(( ${colIndex[$colIndexK+1]} + 1))
+		colIndex[$(( $colIndexK+1 ))]=$(( ${colIndex[$colIndexK+1]} + 1))
 	done
+	#printf "adjusted indices to: \n"
+	#printf "%s\n" ${colIndex[@]}
 
 }
 
 recursiveTreePrint()
 {
-	local CURR_ROW=$1
-	local SIZE=$2
-	local ITER=$3
-	if [[ $CURR_ROW == $(($NROWS - 1)) ]]; then
-		return
-	fi
+	local SIZE=$1
+	local ITER=$2
+	#if [[ $CURR_ROW == $(($NROWS - 1)) ]]; then
+	#	return
+	#fi
 	for ((printIndex=0; printIndex < SIZE; printIndex++))
 	do
-		printRow $NCOLS
+	#	echo 
+		printRow
 	done
 	updateIndices
 	for ((printIndex=0; printIndex < SIZE; printIndex++))
 	do
-		printRow $NCOLS
+		printRow
 		adjustIndices
 	done
 	((SIZE/=2))
 	if [[ $ITER > 1 ]]; then
-		recursiveTreePrint $(($CURR_ROW+1)) $SIZE $(($ITER-1))
+		recursiveTreePrint $SIZE $(($ITER-1))
 	fi
 	#	updateIndices ${colIndex[@]} 
 	#	printRow ${colIndex[@]} $NCOLS
@@ -58,12 +70,17 @@ recursiveTreePrint()
 
 printRow()
 {
-	local NCOLS=$1
 	local colIndexK=0
+	#printf "printing row based on following colindices\n"
 	#printf "%s\n" ${colIndex[@]}
+	#local L=${#colIndex[@]}
 	for ((coli=0; coli < $NCOLS; coli++ ))
 	do
-		if [[ $colIndexK < ${#colIndex[@]} && ${colIndex[$colIndexK]} = $coli  ]]; then
+		#printf "colIndexK = %i\n" $colIndexK
+		#printf "#colIndex[@] = %i\n" $L
+		#printf "colIndex[colIndexK] = %i\n" ${colIndex[$colIndexK]}
+		#printf "coli = %i\n" $coli
+		if [[ ($colIndexK < ${#colIndex[@]}) && (${colIndex[$colIndexK]} = $coli)  ]]; then
 			printf "%i" 1 
 			((colIndexK++))
 		else	
@@ -79,8 +96,9 @@ colIndex[$colIndexK]=49
 firstSize=16
 #printf "before updateIndices\n"
 #printf "%s \n" ${colIndex[@]}
-#updateIndices ${colIndex[@]}
+updateIndices
+updateIndices
 #printf "after updateIndices\n"
 #printf "%s \n" ${colIndex[@]}
-recursiveTreePrint 0 $firstSize 5
+#recursiveTreePrint 0 $firstSize 4
 
